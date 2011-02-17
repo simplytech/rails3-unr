b083c32b847f88584a68f803cabb2baebb916eb9
Alain Achkar <rails@alain.simplytech.com>
2011-02-17 11:47:35 -0500

    Page 16

--- chapter-1/app/controllers/greeting_controller.rb
+++ chapter-1/app/controllers/greeting_controller.rb
@@ -1,5 +1,13 @@
 class GreetingController < ApplicationController
   def index
-    @welcome_message = "Welcome to your first Rails3 application!"
+    @welcome_message = "Welcome to your first rails app"
+    @age = 8
+    @table={
+             "headings" => ["addend", "addend", "sum"],
+             "body"     => [[1,1,2],
+                            [1,2,3],
+                            [1,3,4]] 
+           }
   end
+
 end
