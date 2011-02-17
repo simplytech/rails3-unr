3a00694f8e98de4f3129a9bc75efcb2f0ddb2084
Alain Achkar <rails@alain.simplytech.com>
2011-02-17 11:48:26 -0500

    Page 21

---
+++ photos/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Photos::Application.config.secret_token = '2735b60546ef715b598ec39b9415911ebe337afcdf15e6f42243b554885475c5c73e08aeb14245775a6cd63632533476850ae2162d84aa8132976351d3ca2acd'
